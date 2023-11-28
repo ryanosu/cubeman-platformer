f7de3d146c22f87d6dea3bebf426c30d11e954f9
RyanP <paprockr@oregonstate.edu>
2023-11-27 22:20:21 -0600

    troubleshooted and fixed animation problem and added hair_enemy, which is solid

--- enemies.js
+++ enemies.js
@@ -1,21 +1,64 @@
 // all enemies and their behavior will go here
 
-class Enemies{
+class Enemy{
     constructor(){
+        this.spriteFrameX = 0
+        this.spriteFrameY = 0
+        this.fps = 20
+        this.frameInterval = 1000/this.fps
+        this.frameTimer = 0
     }
 
-    update(){
-
+    update(deltaTime){
+        // movement
+        this.x -= this.enemySpeedX + this.game.gameSpeed
+        //this.y += this.enemySpeedY
+        if(this.frameTimer > this.frameInterval){
+            this.frameTimer = 0
+            if(this.spriteFrameX < this.maxSpriteFrameX)this.spriteFrameX++
+            else this.spriteFrameX = 0
+        }
+        else{
+            this.frameTimer += deltaTime
+        }
     }
-    
-    draw(){
-        
+
+    draw(context){
+        context.drawImage(
+            this.enemyImage, // image that we draw
+            this.spriteFrameX * this.enemyWidth, // origin x-coordinate of top-left corner of source rect 
+            0, // origin y-coordinate of top-left corner of source rect
+            this.enemyWidth, // origin size in width of image
+            this.enemyHeight, // origin size in height of image
+            this.x, // destination x-coordinate on canvas
+            this.y, // destination y-coordinate on canvas
+            this.widthScaled, // destination rectangle width on canvas
+            this.heightScaled // destination rectangle height on canvas
+        )
     }
 }
 
-export class BlueAlien{
-    constructor(){
+export class BlueAlien extends Enemy{
+    constructor(game){
         super()
+        this.game = game
+        this.enemyImage = document.getElementById('hair_enemy')
+        this.enemyWidth = 946 // 12,298 / 13 = 946 
+        this.enemyHeight = 821
+        this.blueAlienScaled = 0.4
+        this.widthScaled = this.enemyWidth * this.blueAlienScaled
+        this.heightScaled = this.enemyHeight * this.blueAlienScaled
+        this.x = this.game.width
+        this.y = this.game.height - this.heightScaled - this.game.groundMargin
+        //this.spriteFrameX = 0
+        //this.spriteFrameY = 1 // WALKING row
+        this.enemySpeedX = 0
+        this.enemySpeedY = 0
+        this.maxSpriteFrameX = 12
+    }
 
+    update(deltaTime){
+        super.update(deltaTime) // runs Enemy.update()
+        this.x -= 1
     }
 }
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { Background } from "./background.js"
 import { Player } from "./player.js"
 import { InputHandler } from "./input.js"
 import { Data } from "./data.js"
+import { BlueAlien } from "./enemies.js"
 
 window.addEventListener('load', function(){
         const canvas = this.document.getElementById('canvas1')
@@ -19,6 +20,9 @@ window.addEventListener('load', function(){
                 this.score = 0
                 this.time = 0
                 this.lives = 3
+                this.enemies = []
+                this.enemyTimer = 0
+                this.enemyInterval = 3000 // ms
                 this.background = new Background(this)
                 this.player = new Player(this)
                 this.input = new InputHandler()
@@ -29,12 +33,36 @@ window.addEventListener('load', function(){
                 this.time += deltaTime
                 this.background.update()
                 this.player.update(this.input.keys, deltaTime)
+                
+                // update enemy timer
+                if (this.enemyTimer > this.enemyInterval){
+                    this.addEnemies()
+                    this.enemyTimer = 0
+                }
+                else {
+                    this.enemyTimer += deltaTime
+                }
+                
+                // update enemies
+                this.enemies.forEach(enemy =>{
+                    enemy.update(deltaTime)
+                })
             }
 
             draw(context){
                 this.background.draw(context)
                 this.player.draw(context)
                 this.data.draw(context)
+
+                this.enemies.forEach(enemy =>{
+                    enemy.draw(context)
+                })
+            }
+
+            addEnemies(){
+                if(this.gameSpeed > 0){
+                    this.enemies.push(new BlueAlien(this))
+                }
             }
         }
 
